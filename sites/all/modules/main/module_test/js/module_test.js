3f5b484cd1ba6fd340020f68d40549b702ad732e
JSK practicas <jctejadasaracho@gmail.com>
2013-10-17 12:49:30 +0200

    Session 6

---
+++ sites/all/modules/main/module_test/js/module_test.js
@@ -0,0 +1,14 @@
+/**
+ * @file module_test.js
+ */
+
+Drupal.behaviors.module_test = {
+  attach: function (context, settings) {
+
+//    alert(Drupal.t('Hello'));
+//    console.log(Drupal);
+//    var obj = 'blablabla';
+//    alert(obj);
+//    alert(Drupal.settings.module_test.variable);
+  }
+};
---
+++ sites/all/modules/main/module_test/tpl/my_page.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file Template for my own url
+ * @param $text This is a text surrounded by a div
+ */
+?>
+
+<div class="this-is-a-class">
+  <?php print $text; ?>
+</div>
